c710bddf02a6cf59dbcf2ff7ecb20a25132785ee
Viral Baitule <viral.baitule@gmail.com>
2017-03-25 09:52:02 -0700

    fixed last character repetation problem

--- Cracking the coding Interview/Arrays and strings/Q1_5.py
+++ Cracking the coding Interview/Arrays and strings/Q1_5.py
@@ -1,6 +1,18 @@
 def string_conpression(string):
-
-
+	length=len(string)
+	count=1
+	check_place=string[0]
+	newstring=''
+	for i in range(1,length):
+		if string[i]==check_place:
+			count+=1
+		else:
+			newstring=newstring+check_place+str(count)
+			check_place=string[i]
+			count=1
+		if i==length-1:
+			newstring=newstring+check_place+str(count)
+	return newstring
 
 
 
