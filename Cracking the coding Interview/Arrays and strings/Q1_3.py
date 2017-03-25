1b1590207633d34f5fed2109d349ebea62beaeaa
Viral Baitule <viral.baitule@gmail.com>
2017-03-25 10:44:48 -0700

    along with finding permutations

--- Cracking the coding Interview/Arrays and strings/Q1_3.py
+++ Cracking the coding Interview/Arrays and strings/Q1_3.py
@@ -1,7 +1,24 @@
+
+def permute_and_check(s1,s2,l,r,check):
+	
+	if (l==r):
+		print (s1)
+		if s1==s2:
+			check="True"	
+	else:
+		for i in range(l,r+1):
+			s1[l],s1[i]=s1[i],s1[l]
+			check=permute_and_check(s1,s2,l+1,r,check)
+			s1[l],s1[i]=s1[i],s1[l]
+	return check
+
 def check_permutation(s1,s2):
-	return
-
-
+	if len(s1)!=len(s2):
+		return False
+	s1=list(s1)
+	s2=list(s2)
+	return(permute_and_check(s1,s2,0,len(s1)-1,"False"))
+	
 
 s1="dog"
 s2="god"
