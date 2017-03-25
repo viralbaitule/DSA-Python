ca7fbefcaefb6b5fdcf098def6a73ca70882f2c7
Viral Baitule <viral.baitule@gmail.com>
2017-03-25 11:17:19 -0700

    import module and test

---
+++ Cracking the coding Interview/LinkedList/Q2_1.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.insert(0, 'C:/Drive/Study/DSA-Python/custom_modules')
+from singly import node,singly
+
+newnode=node()
+newnode.setdata(1)
+newnode.setnext(None)
+print (newnode.getdata())
