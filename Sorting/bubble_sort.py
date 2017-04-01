aa2f928edf5c39044e3ea9f803905ccd92189dc6
Viral Baitule <viral.baitule@gmail.com>
2017-04-01 00:07:08 -0700

    bubble sort

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,9 +1,10 @@
 def bubble_sort(list1):
-	for i in range len(list1):
-		min=i
-		print (i)
-
-
+	for i in range (0,len(list1)-1):
+		for j in range(0,len(list1)-1):
+			if(list1[j]>list1[j+1]):
+				list1[j],list1[j+1]=list1[j+1],list1[j]
 
 list1=[9,3,5,1,4,2]
 bubble_sort(list1)
+for i in list1:
+	print (i)
