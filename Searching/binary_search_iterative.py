226747d613f990aa37240b51cabd999cfd42a4f8
Viral Baitule <viral.baitule@gmail.com>
2017-04-02 12:23:23 -0700

    binary search iterative

--- Searching/binary_search_iterative.py
+++ Searching/binary_search_iterative.py
@@ -1,9 +1,16 @@
 def binary_search_iterative(list1,value):
-	pass
-
-
-
+	low=0
+	high=len(list1)-1
+	while low<=high:
+		mid=low+high//2
+		if value>list1[mid]:
+			print(list1[mid])
+			low=mid
+		elif value<list1[mid]:
+			high=mid-1
+		else:
+			return mid
+	return -1
 
 list1=[4,6,8,13,21,35,38,67]
-binary_search_iterative(list1,35)
-print (list1)
+print(binary_search_iterative(list1,38))
