caf09b475a1c9338cb955d520b431fc3a69563bf
Viral Baitule <viral.baitule@gmail.com>
2017-04-02 09:57:35 -0700

    error in range line 10

--- Sorting/quick_sort.py
+++ Sorting/quick_sort.py
@@ -1,9 +1,22 @@
-def quick_sort(list1):
-	pass
+def quick_sort(list1,start,end):
+	if start<end:
+		pindex=partition(list1,start,end)
+		quick_sort(list1,start,pindex-1)
+		quick_sort(list1,pindex+1,end)
+
+def partition(list1,start,end):
+	pivot=list1[end]
+	pindex=start
+	for i in range(start,end):
+		if list1[i]<=pivot:
+			list1[i],list1[pindex]=list1[pindex],list1[i]
+			pindex-=1
+	list1[pindex],list1[end]=list1[end],list1[pindex]
+	return pindex
 
 
 
 
 list1=[9,3,5,1,4,2]
-quick_sort(list1)
+quick_sort(list1,0,len(list1)-1)
 print (list1)
