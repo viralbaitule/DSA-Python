3eba09c1dff52f7717921ef7b77fb66709f8adb8
Viral Baitule <viral.baitule@gmail.com>
2017-04-02 12:49:05 -0700

    fixed issue : issue was not previous not give () for low+high

--- Searching/binary_search_iterative.py
+++ Searching/binary_search_iterative.py
@@ -2,10 +2,9 @@ def binary_search_iterative(list1,value):
 	low=0
 	high=len(list1)-1
 	while low<=high:
-		mid=low+high//2
+		mid=(low+high)//2
 		if value>list1[mid]:
-			print(list1[mid])
-			low=mid
+			low=mid+1
 		elif value<list1[mid]:
 			high=mid-1
 		else:
@@ -13,4 +12,4 @@ def binary_search_iterative(list1,value):
 	return -1
 
 list1=[4,6,8,13,21,35,38,67]
-print(binary_search_iterative(list1,38))
+print(binary_search_iterative(list1,67))
