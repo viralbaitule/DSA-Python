c1e92a2156957b14f5dcdf411f461dfd3d275219
Viral Baitule <viral.baitule@gmail.com>
2017-04-07 23:05:13 -0700

    infix_to_postfix

--- Stacks and queues/infix_to_postfix.py
+++ Stacks and queues/infix_to_postfix.py
@@ -6,16 +6,43 @@ class stack(object):
 	def push(self,data):
 		self.stk.append(data)
 	def pop (self):
-		return self.stk.pop(data)
+		return self.stk.pop()
 	def peek (self):
 		return self.stk[-1]
 	def isempty(self):
-		return len(stk)==0
+		return len(self.stk)==0
 
 def infix_to_postfix(data):
-	newstack=stack()
+	operator_stack=stack()
+	prec={}
+	prec["/"]=3
+	prec["*"]=3
+	prec["+"]=2
+	prec["-"]=2
+	prec["("]=1
+	oprand_list=[]
+	for i in data:
+		if i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or i in "1234567890":
+			oprand_list.append(i)
+		elif i is "(":
+			operator_stack.push(i)
+		elif i is ")":
+			top=operator_stack.pop()
+			while top!="(":
+				oprand_list.append(top)
+				top=operator_stack.pop()
+		else:
+			while (not operator_stack.isempty()) and prec[operator_stack.peek()]>=prec[i]:
+				oprand_list.append(operator_stack.pop())
+			operator_stack.push(i)
+
+	while not operator_stack.isempty():
+		oprand_list.append(operator_stack.pop())
+	return oprand_list
+
+
 
 
 string="A+B+C*D"
-infix_to_postfix(string)
+print(infix_to_postfix(string))
 		
